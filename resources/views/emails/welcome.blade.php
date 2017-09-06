12c51c80ed2a554cf8d21f8427b42f45efe8bc5e
cheyuwu <frank890417@gmail.com>
2017-09-06 12:18:35 +0800

    update mail html

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,10 +1,11 @@
-<h1>睿田官網聯繫表單通知<h1>
+<h3>睿田官網聯繫表單通知</h3>
 <br>
 填寫時間：{{$time}}<br>
 <hr>
+<p>
 姓名：{{$name}}<br>
 電話：{{$phone}}<br>
 信箱：{{$email}}<br>
 詢問項目：{{$ask_item_name}}<br>
 詢問內容：{{$content}}<br>
-
+</p>
